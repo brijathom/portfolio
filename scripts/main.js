8bf67a2735252986d148b4da8a1ce77fdf219a6b
Brian Thomas <brianjamesthomas@live.com>
2020-09-10 15:04:52 +0100

    Adjust background colors

--- scripts/main.js
+++ scripts/main.js
@@ -10,13 +10,11 @@ function changeTheme(theme) {
     if (theme === "light") {
         document.documentElement.style.setProperty("--background", "rgb(250, 250, 250)");
         document.documentElement.style.setProperty("--secondaryText", "rgb(0, 0, 0)");
-        document.documentElement.style.setProperty("--secondaryBackground", "rgb(211, 211, 211)");
     }
 
     if (theme === "dark") {
         document.documentElement.style.setProperty("--background", "rgb(33 33 33)");
         document.documentElement.style.setProperty("--secondaryText", "rgb(255, 255, 255)");
-        document.documentElement.style.setProperty("--secondaryBackground", "rgb(21 17 17)");
     }
 
     localStorage.setItem("theme", theme);
